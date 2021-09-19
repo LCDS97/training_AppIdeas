a78124571d8eab74668f9e7d90bbf98c74f89fb5
Lucas Conceição dos Santos <lucas.cds1997@gmail.com>
2021-09-18 22:47:04 -0300

    bin2dec completed

--- dashboard/js/bin2dec.js
+++ dashboard/js/bin2dec.js
@@ -1,5 +1,96 @@
-function getValue(){
+function getValue() {
     var binaryNumber = document.getElementById('binaryNumber').value;
-    console.log(binaryNumber);
+    submitOK = "true";
+
+
+    if (binaryNumber == "") {
+        document.getElementById('error').innerHTML = "Must fill with a number";
+        setTimeout(function () {
+
+            document.getElementById('error').innerHTML = "";
+            document.getElementById('binaryNumber').value = "";
+            document.getElementById('disabledTextInput').value = "";
+
+        }, 5000);
+        submitOK = "false";
+
+    }
+
+    if (isNaN(binaryNumber) || binaryNumber.length > 8) {
+        document.getElementById('error').innerHTML = "The field must be a number and less than 8 characters long";
+        setTimeout(function () {
+
+            document.getElementById('error').innerHTML = "";
+            document.getElementById('binaryNumber').value = "";
+            document.getElementById('disabledTextInput').value = "";
+
+        }, 5000);
+
+        submitOK = "false";
+
+    }
+
+    binaryNumber.split('').map((char) => {
+        if (char !== '0' && char !== '1'){
+            document.getElementById('error').innerHTML = "Only 0 or 1 is allowed!";
+            setTimeout(function () {
+
+                document.getElementById('error').innerHTML = "";
+                document.getElementById('binaryNumber').value = "";
+                document.getElementById('disabledTextInput').value = "";
+    
+            }, 5000);
+            submitOK = "false";
+
+        }
+    })
+
+    // if (
+    //     binaryNumber != '0' && binaryNumber != '1'
+    // ) {
+    //     document.getElementById('error').innerHTML = "Only 0 or 1 is allowed!";
+    //     setTimeout(function () {
+
+    //         document.getElementById('error').innerHTML = "";
+    //         document.getElementById('binaryNumber').value = "";
+    //         document.getElementById('disabledTextInput').value = "";
+
+    //     }, 5000);
+    //     submitOK = "false";
+    // }
+
+    // if(binaryNumber.length > 8){
+    //     document.getElementById('error').innerHTML = "Maximum numbers allowed for binary numbers are 8";
+    //     setTimeout(function () {
+
+    //         document.getElementById('error').innerHTML = "";
+
+    //     }, 5000);
+
+    // submitOK = "false";
+    // }
+    
+    if (submitOK == "false"){
+        document.getElementById('binaryNumber').innerHTML = "";
+        document.getElementById('disabledTextInput').disabled = true;
+        return false;
+    }
+
+    if (submitOK == "true"){
+        document.getElementById('disabledTextInput').disabled = false;
+        convertedNumber = parseInt(binaryNumber, 2)
+        document.getElementById('disabledTextInput').value = convertedNumber;
+
+    }
+
+
 
 }
+
+
+
+
+// 1 - Verificar se esta nulo - OK
+// 2 - Somente um ou zero no primeiro - Not
+// 3 - Usuario somente pode digitar até 8 binarios - OK
+// 4 - Usuario verificar no campo o valor do binario convertido - OK
